b3814303d8cf4e86c23dfad9ac7aa663e77012f6
Shehan Nattar <shehan@Shehans-MacBook-Pro.local>
2021-02-11 23:45:37 -0800

    [Menu] Use correct X, Fix stylings for different sized desktop screens, Vertically center links

--- src/assets/MobileMenu.jsx
+++ src/assets/MobileMenu.jsx
@@ -24,14 +24,16 @@ class MobileMenu extends Component {
             <div className={mobileMenuClasses}>
                 <Header theme='light' open={true} toggleMobileMenu={this.toggleMobileMenu.bind(this)} />
                 <div className="mobileMenuLinks">
-                    <a onClick={() => this.goToSlide(2)} href="#"><h2>Property</h2></a>
-                    <a onClick={() => this.goToSlide(4)} href="#"><h2>Amenities</h2></a>
-                    <a onClick={() => this.goToSlide(6)} href="#"><h2>The View</h2></a>
-                    <a onClick={() => this.goToSlide(8)} href="#"><h2>Residences & Penthouses</h2></a>
-                    <a onClick={() => this.goToSlide(13)} href="#"><h2>Availability</h2></a>
-                    <a onClick={() => this.goToSlide(10)} href="#"><h2>Development Team Story</h2></a>
-                    <a onClick={() => this.goToSlide(12)} href="#"><h2>Discover</h2></a>
-                    <a onClick={() => this.goToSlide(15)} href="#"><h2>Contact</h2></a>
+                    <div className="linksWrapper">
+                        <a onClick={() => this.goToSlide(2)} href="#"><h2>Property</h2></a>
+                        <a onClick={() => this.goToSlide(4)} href="#"><h2>Amenities</h2></a>
+                        <a onClick={() => this.goToSlide(6)} href="#"><h2>The View</h2></a>
+                        <a onClick={() => this.goToSlide(8)} href="#"><h2>Residences & Penthouses</h2></a>
+                        <a onClick={() => this.goToSlide(13)} href="#"><h2>Availability</h2></a>
+                        <a onClick={() => this.goToSlide(10)} href="#"><h2>Development Team Story</h2></a>
+                        <a onClick={() => this.goToSlide(12)} href="#"><h2>Discover</h2></a>
+                        <a onClick={() => this.goToSlide(15)} href="#"><h2>Contact</h2></a>
+                    </div>
                 </div>
                 <div className="mobileMenuContact">
                     <div className="socialMedias">
