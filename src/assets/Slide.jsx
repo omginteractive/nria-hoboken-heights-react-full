c0e0f8caa0886dc3501f9dab2b30a59de3c1b2db
Shehan Nattar <shehan@Shehans-MacBook-Pro.local>
2021-02-27 13:11:00 -0800

    [Amenities] Fix dinensions of sections, adjustments to listing to center from edge of page

--- src/assets/Slide.jsx
+++ src/assets/Slide.jsx
@@ -818,11 +818,13 @@ class SlideAmenities extends Component {
         return(
             <>
                 <section className="amenities">
-                    <div className={amenitiesDetailsClasses}>
-                        <h2>Luxury Skyline Front Amenities & Services</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                        <div onClick={this.props.methods.goToContactSlide.bind(this)} className="btn dark">Inquire now</div>
+                    <div className="amenities__details_wrapper">
+                        <div className={amenitiesDetailsClasses}>
+                            <h2>Luxury Skyline Front Amenities & Services</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                            <div onClick={this.props.methods.goToContactSlide.bind(this)} className="btn dark">Inquire now</div>
+                        </div>
                     </div>
                     <div onWheel={this.handleWheelEvent.bind(this)}  className={amenitiesListClasses}>
                         <ul>
