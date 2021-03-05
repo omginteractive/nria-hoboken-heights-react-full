78078c76bd891f21bbc2cd83bd0f0b325c43753e
Shehan Nattar <shehan@Shehans-MacBook-Pro.local>
2021-03-04 16:02:15 -0800

    [Neighborhood] Removed buttons

--- src/assets/Slide.jsx
+++ src/assets/Slide.jsx
@@ -550,10 +550,10 @@ class SlideNeighborhoodCommunity extends Component {
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                         </div>
-                        <div className="button_container">
+                        {/* <div className="button_container">
                             <div className="btn light">Lorem ipsum</div>
                             <div className="btn light">Lorem ipsum</div>
-                        </div>
+                        </div> */}
                     </div>
                 </section>
             </>
