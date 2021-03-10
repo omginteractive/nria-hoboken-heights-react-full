baa4139ba2ff94fcea51cbf6aa2204af7c0f22ff
Shehan Nattar <shehan@Shehans-MacBook-Pro.local>
2021-03-10 12:47:25 -0800

    [Map] Remove mobile touch visual highlight on button and text

--- src/assets/Slide.jsx
+++ src/assets/Slide.jsx
@@ -270,8 +270,8 @@ class SlideMap extends Component {
                         <img src={require('./images/map/Motion_logo.gif').default} alt="" className="map not-mobile"/>
                         <img src={require('./images/map/Motion_logo_mobile_animateonce.gif').default} alt="" className="map mobile-only"/>
                     </div>
-                    <div onClick={this.toggleMap.bind(this)} className="satelliteToggle vertical_toggle_column">
-                        <div className="rotatedText">{toggleBarText}</div>
+                    <div onClick={this.toggleMap.bind(this)} className="satelliteToggle vertical_toggle_column noSelect">
+                        <div className="rotatedText noSelect">{toggleBarText}</div>
                     </div>
                     <div className="mapSection__locationList">
                         {locationListings.map((amenity, idx) => {
