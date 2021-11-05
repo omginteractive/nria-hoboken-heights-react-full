ae0498dc2bd4468612c612a145c6dfbabacfee46
eduhdev <eduardocarvalho970@gmail.com>
2021-11-05 11:27:45 -0300

    #26510 - fix zoom component

--- src/assets/slideComponents/ImageZoom.jsx
+++ src/assets/slideComponents/ImageZoom.jsx
@@ -1,24 +1,40 @@
+import { useEffect, useState } from 'react'
 import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch'
 
 const ImageZoom = ({ image }) => {
+  const [initialPosition, setInitialPosition] = useState(null)
+
+  useEffect(() => {
+    const img = 612
+    const width = window.innerWidth
+    const calcWidth = width > img ? width - img : 6;
+    setInitialPosition(Math.floor(calcWidth / 2))
+  }, [])
+
   return (
-    <TransformWrapper initialScale={1} centerOnInit={true}>
-      {({ zoomIn, zoomOut }) => (
-        <>
-          <div className="tools">
-            <button onClick={() => zoomIn()}>+</button>
-            <button onClick={() => zoomOut()}>-</button>
-          </div>
-          <TransformComponent>
-            <img
-              src={image}
-              alt="full floorplan"
-              className="full_floorplan_image"
-            />
-          </TransformComponent>
-        </>
-      )}
-    </TransformWrapper>
+    initialPosition && (
+      <TransformWrapper
+        initialScale={1}
+        initialPositionX={initialPosition}
+        maxScale={3}
+      >
+        {({ zoomIn, zoomOut }) => (
+          <>
+            <div className="tools">
+              <button onClick={() => zoomIn()}>+</button>
+              <button onClick={() => zoomOut()}>-</button>
+            </div>
+            <TransformComponent>
+              <img
+                src={image}
+                alt="full floorplan"
+                className="full_floorplan_image"
+              />
+            </TransformComponent>
+          </>
+        )}
+      </TransformWrapper>
+    )
   )
 }
 
